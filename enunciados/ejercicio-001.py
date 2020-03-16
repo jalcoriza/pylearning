8bb248814ec1f65ad424ae48641b6f3bf77607d1
Javier Alcoriza <jacoriza@gmail.com>
2020-03-16 20:51:48 +0100

    Completo enunciado ejercicio 001

--- enunciados/ejercicio-001.py
+++ enunciados/ejercicio-001.py
@@ -7,3 +7,11 @@ Las respuestas se deberán almacenar en una lista.
 La salida del programa debe ser la media y la desviación típica de la variable aleatoria "altura"
 """
 
+print('Enunciado ejercicio #001')
+print('------------------------')
+print('Escribir un programa que lea 10 números enteros desde el teclado simulando una encuesta que pregunte a')
+print('una muestra de 10 personas "¿Cuánto mide (cm)?')
+print('Las respuestas se deberán almacenar en una lista.')
+print('La salida del programa debe ser la media y la desviación típica de la variable aleatoria "altura"')
+
+
