4575e3ce5d4609b4d8bf759db0018089a4027405
Javier Alcoriza <jacoriza@gmail.com>
2020-03-16 20:17:44 +0100

    Incluido primer enunciado

---
+++ enunciados/ejercicio-001.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Escribir un programa que lea 10 números enteros desde el teclado simulando una encuesta que pregunte a
+una muestra de 10 personas "¿Cuánto mide (cm)?
+Las respuestas se deberán almacenar en una lista.
+La salida del programa debe ser la media y la desviación típica de la variable aleatoria "altura"
+"""
+
