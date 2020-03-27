68745048963f249200ae06042b564c8bd0440637
Javier Alcoriza <jacoriza@gmail.com>
2020-03-27 10:22:17 +0100

    Added ejemplos folder

---
+++ ejemplos/ejemplo-001.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Elementary python use samples
+print("Hello")
+
+# Data classes
+print("The type of '3.2' is", type(3.2))
+
+# Type conversion int()/float()/str()
+print("The pi number is", float('3.1415'))
+
+# Conventions
+# i) variables begin with a small letter
+# ii) constants are written in capital letters
+PI = 3.1415
+r1 = 25.54
+perimeter = 2*PI*r1
+
+print("The perimeter of a circle with radius of 25.54cm is equal to", perimeter, "cm")
+
+decimal = 3 + len("hi")
+print(decimal)
+
+
+
+
+
+
+
